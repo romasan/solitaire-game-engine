6c8b9e4ab7d47feb2345265cf73d63980ceaf8ad
roman <kotapesic@gmail.com>
2016-06-16 10:46:30 +0300

    Fix удалил лишние файлы

--- sources/autosteps/addAutoStep.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import fallAutoStep from 'fallAutoStep';
-
-export default {
-	fallAutoStep
-};
--- sources/autosteps/autoSteps.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default class {
-	
-	constructor(params) {
-
-		this.groups = params.groups;
-	}
-} 
