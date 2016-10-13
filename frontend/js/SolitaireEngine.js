818c6228d80b8df3ffa2bc666dce887e2c5a1927
roman <kotapesic@gmail.com>
2016-10-13 21:22:51 +0000

    add test page

--- frontend/js/SolitaireEngine.js
+++ frontend/js/SolitaireEngine.js
@@ -124,7 +124,7 @@ var SolitaireEngine =
 	exports.options = _defaults2.default;
 	exports.winCheck = _winCheck2.default.hwinCheck;
 	exports.generator = _deckGenerator2.default;
-	exports.version = (9091491666).toString().split(9).slice(1).map(function (e) {
+	exports.version = (9091491677).toString().split(9).slice(1).map(function (e) {
 		return parseInt(e, 8);
 	}).join('.');
 	
@@ -4794,7 +4794,7 @@ var SolitaireEngine =
 		}, {
 			key: 'get',
 			value: function get() {
-				var reset = arguments.length <= 0 || arguments[0] === undefined ? true : arguments[0];
+				var reset = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
 	
 	
 				var _req = this.steps;
